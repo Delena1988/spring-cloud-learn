bd12a9a0cf6b7485ea27c8fb5e520ab94e17a1c3
林剑 <linjian19881026@gmail.com>
2019-03-18 09:20:26 +0800

    eureka-client init

---
+++ eureka-client/src/main/java/com/linjian/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,21 @@
+package com.linjian.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @param:
+ * @return:
+ * @author: jlin
+ * @date: 2019-03-16 16:27
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+}
--- eureka-server/src/main/java/com/linjian/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/linjian/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,17 @@ package com.linjian.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+/**
+ * @param:
+ * @return:
+ * @author: jlin
+ * @date: 2019-03-16 15:04
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
