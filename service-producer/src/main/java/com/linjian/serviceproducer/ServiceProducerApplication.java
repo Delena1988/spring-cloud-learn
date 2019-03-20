63326f6361b57d9ae01e48d0457b9150b91d858f
林剑 <linjian19881026@gmail.com>
2019-03-20 10:20:24 +0800

    service-producer service-consumer

--- service-producer/src/main/java/com/linjian/serviceproducer/ServiceProducerApplication.java
+++ service-producer/src/main/java/com/linjian/serviceproducer/ServiceProducerApplication.java
@@ -2,8 +2,10 @@ package com.linjian.serviceproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceProducerApplication {
 
 	public static void main(String[] args) {
