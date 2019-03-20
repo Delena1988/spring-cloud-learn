891e3ea32544f4300688bc730fa497b8b90ab2d4
林剑 <linjian19881026@gmail.com>
2019-03-20 16:09:54 +0800

    actuator

--- config-client/src/main/java/com/linjian/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/linjian/configclient/ConfigClientApplication.java
@@ -2,7 +2,18 @@ package com.linjian.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @param:
+ * @return:
+ * @author: jlin
+ * @date: 2019-03-20 15:18
+ */
+//激活注册发现
+@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigClientApplication {
 
--- config-server/src/main/java/com/linjian/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/linjian/configserver/ConfigServerApplication.java
@@ -2,10 +2,21 @@ package com.linjian.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @param:
+ * @return:
+ * @author: jlin
+ * @date: 2019-03-20 15:16
+ */
 //开启配置服务器
 @EnableConfigServer
+//激活注册发现
+@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
